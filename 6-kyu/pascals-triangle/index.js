83e58f65d9cc7fbe4b3861ed2b3e1dd2c873754c
NIYOMUGISHA Obed <obed6@duck.com>
2026-07-28 10:23:32 +0200

    Add solution for 'pascals triangle' kata

---
+++ 6-kyu/pascals-triangle/index.js
@@ -0,0 +1,16 @@
+function pascalsTriangle(n) {
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    const prev = (i - 1) * i / 2;
+    result.push(1);
+    for (let j = 1; j < i; j++) {
+      result.push(result[prev + j - 1] + result[prev + j]);
+    }
+    if (i > 0) result.push(1);
+  }
+
+  return result;
+}
+
+console.log(pascalsTriangle(10));
